1fe3b7b7bd3f0c4ff77d00513949ae4bcc9aa77a
29 <cukamoto@mail.ru>
2023-08-01 09:52:02 +0300

    Add first code

---
+++ tasks.py
@@ -0,0 +1,26 @@
+import time
+
+def calculator (a,b):
+    summa = a + b
+    res = a*b
+    if res >= 1000:
+        print (res)
+    else:
+        print(summa)
+        
+print("Добро пожаловать в калькулятор")      
+a = int(input("Введите число:"))  
+b = int(input("Введите число:"))
+print ("Подождите...........")
+time.sleep(3)
+calculator(a,b)
+
+     
+    
+
+
+
+
+
+
+
