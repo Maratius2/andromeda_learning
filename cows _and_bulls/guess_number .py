0f44d3eb47dcfd5befaf34497095969a203cc377
29 <cukamoto@mail.ru>
2023-08-04 14:55:32 +0300

    написал игру угадай число

--- cows _and_bulls/guess_number .py
+++ cows _and_bulls/guess_number .py
@@ -6,36 +6,25 @@ import random
 
 def game(update: Update, context: CallbackContext):
     user_name = update._effective_user.first_name
-
-
-        
-def gateway(update: Update, context: CallbackContext):
-    user_name = update._effective_user.first_name
-    my_word = update.message.text
+    my_number = update.message.text
     if "секрет" not in context.user_data:
-        with open("cows _and_bulls/words.txt", encoding="utf-8") as file:
-            words = file.read().split("\n")
-        secret_word = random.choice(words)
-        context.user_data["секрет"] = secret_word #записываем
+        secret_number = random.randint(1, 100)
+        context.user_data["секрет"] = secret_number
     else:
-        secret_word = context.user_data["секрет"]  #достаем
-    if len(secret_word) != len(my_word):
-        update.message.reply_text(f"Количество букв должно быть{len(secret_word)}")   
-        return None 
-    bulls = 0 
-    cows = 0
-    for index, letter in enumerate(my_word):
-        if letter in secret_word:
-            if secret_word[index] == my_word[index]:
-                bulls+= 1
-            else:
-                cows+= 1
-    update.message.reply_text(f"В вашем слове {bulls} быков и {cows} коров")
-    if bulls == len(secret_word):
+        secret_number = context.user_data["секрет"]
+    #здесь должна быть проверка
+    my_number = int(my_number)
+    if my_number > secret_number:
+        update.message.reply_text(f"Мое число меньше")
+    elif my_number < secret_number:
+        update.message.reply_text(f"Мое число ,больше")
+    else:
         update.message.reply_photo("https://w7.pngwing.com/pngs/815/457/png-transparent-league-of-legends-victory-league-of-legends-riven-command-conquer-generals-age-of-empires-video-game-victory-miscellaneous-blue-game.png")
+        del context.user_data["секрет"]
         
+    
         
-message_handler = MessageHandler(Filters.text, gateway)
+message_handler = MessageHandler(Filters.text, game)
 
 #Сам бот и его зам.
 updater = Updater(TOKEN)
