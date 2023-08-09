96ed6ff03a6ee66d7f0e509df54c1dcd6957fe88
29 <cukamoto@mail.ru>
2023-08-09 14:57:34 +0300

    добавили валюту и стикеры

--- cows_and_bulls_update/constans.py
+++ cows_and_bulls_update/constans.py
@@ -1,8 +1,26 @@
+from telegram import  ReplyKeyboardMarkup
+import pymorphy2
+
 GO = "НАЧАТЬ"
 LEVEL, FIRST, CLUE = range(3)
-EASY, MEDIUM, HARD = "easy", "medium", "hard"
+morph = pymorphy2.MorphAnalyzer()
+COW = morph.parse("корова")[0]
+BULL = morph.parse("бык")[0]
+
+
+
+EASY, MEDIUM, HARD, CANCEL, BALANCE = "easy", "medium", "hard","/cancel", "Баланс"
 LEVELS = {
     EASY: 3,
     MEDIUM: 4,
     HARD: 5
 }
+
+
+
+KEYPAD = ReplyKeyboardMarkup([[CANCEL],[BALANCE]],
+                            resize_keyboard=True,
+                            input_field_placeholder="Чтобы сдаться нажми на /cancel. Нажми на Баланс, если хочешь увидеть баланс монет")
+       
+MEDIUM_PRICE = 50
+HARD_PRICE = 100
